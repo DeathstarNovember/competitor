cd0125e4de70159f51b12ac190d55eaf3bec6a5d
DeathstarNovember <johnathon.hill@gmail.com>
2019-11-02 12:32:02 -0400

    Make Stat summary collapsable

--- src/components/StatSummary.tsx
+++ src/components/StatSummary.tsx
@@ -1,72 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import { Entry } from "../types";
 import { entryGroupCalculations, getEntryPace } from "../util";
 import { format, parseISO } from "date-fns";
+import { MdSwapVert } from "react-icons/md";
 type StatSummaryProps = {
   entries: Entry[];
 };
 const StatSummary: React.FC<StatSummaryProps> = ({ entries }) => {
   const statSummary = entryGroupCalculations(entries);
+  const [displayFullSummary, setDisplayFullSummary] = useState(false);
   return (
     <div className="max-w-md mx-auto">
-      <div className="text-xl font-bold">
-        {`${entries
-          .map(e => `${e.user.firstName} ${e.user.lastName[0]}.`)
-          .filter((v, i, a) => a.indexOf(v) === i)
-          .join(", ")} ${format(
-          parseISO(
-            String(
-              entries
-                .map(e => e.completedAt)
-                .sort((a, b) => Number(b) - Number(a))[entries.length - 1]
-            )
-          ),
-          "MM/dd/yy"
-        )} - ${format(
-          parseISO(
-            String(
-              entries
-                .map(e => e.completedAt)
-                .sort((a, b) => Number(b) - Number(a))[0]
-            )
-          ),
-          "MM/dd/yy"
-        )}`}
-      </div>
-      <div className="bg-blue-700 rounded-lg p-2 text-white">
-        <div className="flex flex-1 mb-3 justify-around">
-          <div className="flex-column">
-            <div className="font-bold">Total Dist</div>
-            <div className="font-bold">{`${statSummary.totalDistance}m`}</div>
-          </div>
-          <div className="flex-column">
-            <div className="font-bold">Avg Dist</div>
-            <div className="font-bold">{`${statSummary.averageDistance.toFixed(
-              2
-            )}m`}</div>
-          </div>
-          <div className="flex-column">
-            <div className="font-bold">Avg Spd</div>
-            <div className="font-bold">{`${statSummary.avgPace}m/s`}</div>
-          </div>
+      <div>
+        <div className="text-xl font-bold flex flex-1 justify-between">
+          {`Summary since ${format(
+            parseISO(
+              String(
+                entries
+                  .map(e => e.completedAt)
+                  .sort((a, b) => Number(b) - Number(a))[entries.length - 1]
+              )
+            ),
+            "MM/dd/yy"
+          )} - ${format(
+            parseISO(
+              String(
+                entries
+                  .map(e => e.completedAt)
+                  .sort((a, b) => Number(b) - Number(a))[0]
+              )
+            ),
+            "MM/dd/yy"
+          )}`}
+          <button
+            className="bg-gray-500 hover:bg-green-700 text-black font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            onClick={() => setDisplayFullSummary(!displayFullSummary)}
+          >
+            <MdSwapVert />
+          </button>
         </div>
-        <div className="flex flex-1 justify-around">
-          <div className="flex-column">
-            <div className="font-bold">Fastest Spd</div>
-            <div className="font-bold">{`${getEntryPace(
-              statSummary.fastestEntry
-            )}m/s`}</div>
+      </div>
+      <div className="p-2">
+        <div className="flex flex-col mb-3">
+          <div className="flex flex-1 justify-between">
+            <div className="">Total Dist</div>
+            <div className="">{`${statSummary.totalDistance}m`}</div>
           </div>
-          <div className="flex-column">
-            <div className="font-bold">Avg Pwr</div>
-            <div className="font-bold">{`${statSummary.avgPowerOutput}W`}</div>
-          </div>
-          <div className="flex-column">
-            <div className="font-bold">Total Pwr</div>
-            <div className="font-bold">{`${(
-              statSummary.totalPowerProduced / 1000
-            ).toFixed(2)}kJ`}</div>
-          </div>
+          {displayFullSummary ? (
+            <>
+              <div className="flex flex-1 justify-between">
+                <div className="">Avg Dist</div>
+                <div className="">{`${statSummary.averageDistance.toFixed(
+                  2
+                )}m`}</div>
+              </div>
+              <div className="flex flex-1 justify-between">
+                <div className="">Avg Spd</div>
+                <div className="">{`${statSummary.avgPace}m/s`}</div>
+              </div>
+              <div className="flex flex-1 justify-between">
+                <div className="">Fastest Spd</div>
+                <div className="">{`${getEntryPace(
+                  statSummary.fastestEntry
+                )}m/s`}</div>
+              </div>
+              <div className="flex flex-1 justify-between">
+                <div className="">Avg Pwr</div>
+                <div className="">{`${statSummary.avgPowerOutput}W`}</div>
+              </div>
+              <div className="flex flex-1 justify-between">
+                <div className="">Total Pwr</div>
+                <div className="">{`${(
+                  statSummary.totalPowerProduced / 1000
+                ).toFixed(2)}kJ`}</div>
+              </div>
+            </>
+          ) : null}
         </div>
       </div>
     </div>
